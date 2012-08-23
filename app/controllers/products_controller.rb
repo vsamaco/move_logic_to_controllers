b6c395018c2ebd84af84835fc247bdf0b7a412d3
Vincent Samaco <vsamaco@gmail.com>
2012-08-22 20:33:42 -0700

    updated routes, setup update/destroy

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -13,16 +13,49 @@ class ProductsController < ApplicationController
     
     respond_to do |format|
       if @product.save
-        format.html {render :action => "create"}
-        format.json {render :json => @product}
+        format.html { render :action => "create" }
+        format.json { render :json => @product }
       else
-        format.html {render :action => "new"}
-        format.json {render :json => @product.errors, :status => :unprocessable_entity}
+        format.html { render :action => "new" }
+        format.json { render :json => @product.errors, :status => :unprocessable_entity }
       end
     end
   end
   
   def new
     @product = Product.new
+  end
+  
+  def show
+    @product = Product.find(params[:id])
+  end
+  
+  def edit
+    @product = Product.find(params[:id])
+  end
+  
+  def update
+    @product = Product.find(params[:id])
+    
+    respond_to do |format|
+      if @product.update_attributes(params[:product])
+        format.html {redirect_to @product, :notice => 'Product was successfully updated'}
+        format.json { head :no_content }
+      else
+        format.html {render :action => 'edit' }
+        format.json {render :json => @product.errors, :status => :unprocessable_entity }
+      end
+    end
+  end
+  
+  def destroy
+    @product = Product.find(params[:id])
+    
+    respond_to do |format|
+      if @product.destroy
+        format.html { redirect_to products_path, :notice => 'Product was successfully deleted' }
+        format.json { head :no_content }
+      end
+    end
   end
 end
